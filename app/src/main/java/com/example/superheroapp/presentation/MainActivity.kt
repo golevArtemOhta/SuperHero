f4f3cd1ab233bc0ee1176bec4a808465833a88f3
golevArtemOhta <93866339+golevArtemOhta@users.noreply.github.com>
2024-04-01 14:41:35 +0200

    add fragment_main.xml and item_super_hero.xml

--- app/src/main/java/com/example/superheroapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.superheroapp
+package com.example.superheroapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.superheroapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
